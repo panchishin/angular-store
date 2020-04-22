ffa8a20e8a4a75424c95b763c8b5a6c2579c28ea
Anton Panchishin <anton.panchishin@gmail.com>
2020-04-22 16:03:22 -0400

    add 4th product

--- src/app/products.ts
+++ src/app/products.ts
@@ -13,6 +13,11 @@ export const products = [
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    name: 'The Brick',
+    price: 199,
+    description: 'It makes a good paper weight'
   }
 ];
 
